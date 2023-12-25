11b0d29a2955f1b1e4a7b35e7e40490bc0d09ecf
Jschles1 <johnschlesinger5@gmail.com>
2023-12-24 19:15:23 -0500

    Finish category header

--- src/app/(shopping)/[categoryName]/category-header.tsx
+++ src/app/(shopping)/[categoryName]/category-header.tsx
@@ -4,7 +4,7 @@ export default function CategoryHeader({
   categoryName: string;
 }) {
   return (
-    <h1 className="bg-black text-white uppercase text-center font-bold text-[1.75rem] py-8">
+    <h1 className="bg-black text-white uppercase text-center font-bold text-[1.75rem] md:text-[2.5rem] py-8 md:pt-[6.563rem] md:pb-[6.063rem]">
       {categoryName}
     </h1>
   );
