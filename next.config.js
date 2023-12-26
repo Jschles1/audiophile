6468a21188fb782581b1d589891a145b9ba6d258
Jschles1 <johnschlesinger5@gmail.com>
2023-12-26 16:26:33 -0500

    Add cloudflare domain

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    // remotePatterns: ["imagedelivery.net"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "imagedelivery.net",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
