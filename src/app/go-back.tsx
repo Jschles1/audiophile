0d72d4698a6988522394ffab357f705952d29951
Jschles1 <johnschlesinger5@gmail.com>
2023-12-26 16:30:05 -0500

    Fix padding on go back

--- src/app/go-back.tsx
+++ src/app/go-back.tsx
@@ -4,9 +4,9 @@ import { Button } from "@/components/ui/button";
 
 export default function GoBack() {
   return (
-    <div className="mx-auto max-w-[1110px] px-6">
+    <div className="mx-auto max-w-[1110px] px-6 pt-4 py-6">
       <Button
-        className="relative px-0 pt-4 py-6 capitalize font-normal text-[0.938rem] leading-[1.563rem] text-black hover:text-raw-sienna text-opacity-50"
+        className="relative px-0 capitalize font-normal text-[0.938rem] leading-[1.563rem] text-black hover:text-raw-sienna text-opacity-50"
         aria-label="Go Back"
         variant="ghost"
         onClick={() => window.history.back()}
