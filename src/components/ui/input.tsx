81480fd15de3921d18425d3f2d97068e47bd275d
Jschles1 <johnschlesinger5@gmail.com>
2023-12-28 17:18:08 -0500

    Fix input border radius

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-[56px] w-full rounded-md border border-input-border bg-white px-6 py-[1.125rem] text-sm focus-visible:outline-none focus-visible:border-raw-sienna focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          "flex h-[56px] w-full rounded-lg border border-input-border bg-white px-6 py-[1.125rem] text-sm focus-visible:outline-none focus-visible:border-raw-sienna focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
         ref={ref}
