2316b0d1623a9bed37f5bccef7e8da30af9893d0
Jschles1 <johnschlesinger5@gmail.com>
2024-01-31 11:41:44 -0500

    Preload hero images

--- src/app/(shopping)/page.tsx
+++ src/app/(shopping)/page.tsx
@@ -2,14 +2,34 @@ import Hero from "../home/hero";
 import AboutAudiofile from "./about-audiofile";
 import Categories from "./categories";
 import Banners from "../home/banners";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="w-full bg-alabaster">
-      <Hero />
-      <Categories />
-      <Banners />
-      <AboutAudiofile />
-    </div>
+    <>
+      <Head>
+        <link
+          rel="preload"
+          href="/public/assets/home/desktop/image-hero.jpg"
+          as="image"
+        />
+        <link
+          rel="preload"
+          href="/public/assets/home/mobile/image-header.jpg"
+          as="image"
+        />
+        <link
+          rel="preload"
+          href="/public/assets/home/tablet/image-header.jpg"
+          as="image"
+        />
+      </Head>
+      <div className="w-full bg-alabaster">
+        <Hero />
+        <Categories />
+        <Banners />
+        <AboutAudiofile />
+      </div>
+    </>
   );
 }
